fc61b27bdb3cde3baca080f52c931d3050e1f373
jabinstech <jabinstech.net@gmail.com>
2025-01-11 15:55:34 -0500

    Add Bot Code + README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # WatchBot
-Simple Discord bot to monitor a website's uptime and latency. Used for monitoring @hatchdotlol's API.
+Simple Discord bot to monitor a website's uptime and latency. *Used for monitoring [hatch.lol](https://github.com/hatchdotlol)'s API.  
+## Setup
+1. Install Python if you don't already have it - [download](https://python.org)
+2. Install dependencies with `pip install discord.py requests`
+3. Configure your bot. Open `config.json` and edit the URL you want to monitor, the wait time (15 seconds by default), the command prefix (if desired), your bot's token (see **Creating Your Bot**), and the role ID of the role you want to be able to run the commands. *I recommend keeping setting it to your administrator or owner role so users don't disable the monitoring.*
+4. Run `bot.py`: use `py bot.py` in your terminal on Windows or `python3 bot.py` on Linux.
+5. Begin monitoring: type `!begin` in the channel you'd like the bot to send updates to. *If you changed the prefix in the configuration file, use that instead of !*
+## Creating Your Bot
+If you don't have a Discord bot application set up, this section helps you do it.
+1. Log in to the [Discord Developer Portal](https://discord.com/developers/applications)
+2. Click the button in the top right that says New Application
+3. Name the bot and agree to the terms. Then click create.
+4. Click on the **Bot** tab on the left, scroll down to **Token** and then click **Reset Token**.
+5. Copy the token and store it somewhere safe *and private*.
+6. Scroll down to **Privileged Gateway Intents** and enable **Server Members Intent** and **Message Content Intent**.
+7. On the left sidebar, click on the **OAuth2** tab, scroll down to **OAuth2 URL Generator** and choose **Bot**, then in the new menu that appears below choose **Administrator** (you may want to select individual permissions instead).
+> [!CAUTION]
+> Enabling the Administrator permission gives your bot access to everything in whichever server you add it to, which could be problematic if you do not store your token securely. Also, if you do not own the server you are adding the bot to, you may need to select individual permissions instead, such as *Send Messages*, *Manage Messages*, *Embed Links*, *Attach Files*, *Add Reactions*, *View Channels*, and *Manage Server* if you want it to be able to work in private channels.
+8. Select Guild Install from the dropdown after you've selected the bot permissions, and copy the generated URL. Then paste it into a new tab and add the bot to your server.
+9. Select the **Installation** tab on the left side and uncheck the **User Install** box. Then, use the dropdown below to set **Install Link** to **None**
+10. Select the **Bot** tab again, and toggle **Public Bot** off.
+Your bot then should be ready to go! Steps 9 and 10 go through setting your bot as private so people can't add it to their own server. You can now start the bot setup detailed above.
---
+++ bot.py
@@ -0,0 +1,162 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from discord.ui import Button, View
+import time
+import requests
+import asyncio
+import math
+import json
+
+# Load configuration from config.json
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+TOKEN = config['token']
+url = config['url']
+waitTime = config['waitTime']
+version = config['version']
+command_prefix = config['command_prefix']
+required_role_id = config['required_role_id']
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.reactions = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+bot = commands.Bot(command_prefix=command_prefix, intents=intents)
+
+watching = False
+
+def truncate(number, decimal_places):
+    factor = 10.0 ** decimal_places
+    return math.trunc(number * factor) / factor
+
+def getPlatformLatency():
+    begin = time.time()
+    response = requests.get(url)
+    end = time.time()
+    total = truncate(((end - begin) * 1000), 2)
+    return total
+
+def getPlatformStatus():
+    response = requests.get(url)
+    return response.status_code
+
+def statusColor():
+    latency = getPlatformLatency()
+    if getPlatformStatus() == 200:
+        if latency < 16:
+            return discord.Color.magenta()
+        elif latency >= 16 and latency < 180:
+            return discord.Color.blue()
+        elif latency >= 180 and latency < 360:
+            return discord.Color.green()
+        elif latency >= 360 and latency < 580:
+            return discord.Color.yellow()
+        else:
+            return discord.Color.red()
+    else:
+        return discord.Color.dark_red()
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command(name="stop")
+async def stop(ctx):
+    required_role = discord.utils.get(ctx.guild.roles, id=required_role_id)
+    if required_role in ctx.author.roles:
+        global watching
+        watching = False
+        print(f"API monitoring stopped by {ctx.author.name} / {ctx.author.mention}")
+        await ctx.send("Stopped API uptime monitoring.")
+        await ctx.message.delete()
+    else:
+        await ctx.reply("❌ You're not allowed to do that.")
+
+@bot.command(name="waittime")
+async def awt(ctx, arg: int):
+    required_role = discord.utils.get(ctx.guild.roles, id=required_role_id)
+    if required_role in ctx.author.roles:
+        global waitTime
+        waitTime = arg
+        print(f"Wait Time set to {waitTime} by {ctx.author.name} / {ctx.author.mention}")
+        await ctx.send(f"Updated API ping time to {arg} seconds")
+        await ctx.message.delete()
+    else:
+        await ctx.reply("❌ You're not allowed to do that.")
+
+@bot.command(name="begin")
+async def beginwatch(ctx):
+    global watching, url, waitTime
+    required_role = discord.utils.get(ctx.guild.roles, id=required_role_id)
+    if required_role in ctx.author.roles:
+        if not watching:
+            watching = True
+            channel = ctx.message.channel
+            await ctx.message.delete()
+            
+            latency = getPlatformLatency()
+            status_code = getPlatformStatus()
+
+            if status_code == 200:
+                status = "UP"
+            else:
+                status = "DOWN"
+
+            beginEmbed = discord.Embed(
+                title="Begun monitoring API",
+                description=f"Current status: **{status}**\nStatus code: **{status_code}**\nAPI latency: **{latency}** ms\nBot latency: **{round(bot.latency*1000)}** ms\nUpdating every **{waitTime} seconds**.",
+                color = statusColor()
+            )
+            beginEmbed.set_footer(text=f"WatchBot {version}", url="https://github.com/jab11n/watchbot")
+            await channel.send(embed=beginEmbed)
+
+            status_old = " "
+
+            while watching:
+                await asyncio.sleep(waitTime)
+
+                latency = getPlatformLatency()
+                status_code = getPlatformStatus()
+                
+                if status_code == 200:
+                    status = "UP"
+                else:
+                    status = "DOWN"
+                
+                print("WatchBot: | " + str(time.time()) + " | " + status)
+                
+                if status_old != status:
+                    if status_code == 200:
+                        embed = discord.Embed(
+                            title=f"✅ {url} is UP",
+                            description=f"Request to {url} succeeded with status code {status_code}.",
+                            color=discord.Color.green()
+                        )
+                        embed.set_footer(text=f"Response time: {latency} ms | Bot ping: {round(bot.latency*1000)} ms | WatchBot {version}", url="https://github.com/jab11n/watchbot")
+                        await channel.send(embed=embed)
+                    else:
+                        embed = discord.Embed(
+                            title=f"❌ {url} is DOWN",
+                            description=f"Request to {url} FAILED ({status_code}).",
+                            color=discord.Color.red()
+                        )
+                        embed.set_footer(text=f"Request latency : {latency} ms | Bot ping: {round(bot.latency*1000)} ms | WatchBot {version}", url="https://github.com/jab11n/watchbot")
+                        await channel.send(embed=embed)
+                status_old = status
+        else:
+            await ctx.send("❌ I'm already doing that")
+            await ctx.message.delete()
+    else:
+        await ctx.reply("❌ You're not allowed to do that.")
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send(f"Bot latency: `{round(bot.latency*1000)}` ms\nPlatform latency: `" + getPlatformLatency + "` ms")
+    await ctx.message.delete()
+
+bot.run(TOKEN)
